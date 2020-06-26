12e8aa84be30a1c05c959be9d4b0ddc2dd744137
Efren Carrillo <efren.sistemas@gmail.com>
2020-06-25 21:51:04 -0500

    login

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -25,7 +25,7 @@
             <a class="nav-link" href="/cart">Carrito</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
+            <a class="nav-link" href="/login">Login</a>
         </li>
         <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
