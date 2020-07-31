d9db9d53e467ff4ce9f5b28a1b5051e89ec747d1
Efren Carrillo <efren.sistemas@gmail.com>
2020-07-30 19:11:02 -0500

    fix data

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -39,12 +39,36 @@ class UserSeeder extends Seeder
             'user_id'           => $user->id
         ]);
 
+        $user = User::create([
+            'name' => 'Felicity Huaranca',
+            'email' => 'felihuarancca2@gmail.com',
+            'password' => Hash::make('felihuarancca'),
+            'type_user_id' => 3
+        ]);
+
+        Customer::create([
+            'first_name'        => 'Felicity',
+            'last_name'         => 'Huaranca',
+            'document_type'     => 2,
+            'document_number'   => '99999999',
+            'phone'             => '999999999',
+            'departament'       => 0,
+            'province'          => 0,
+            'district'          => 0,
+            'address'           => 'Av Ejemplo',
+            'number'            => '',
+            'legal'             => 1,
+            'tyc'               => 1,
+            'active'            => 1,
+            'user_id'           => $user->id
+        ]);
+
         // creación de usuario
         $user = User::create([
             'name' => 'Efren Carrillo',
             'email' => 'edarcarto@gmail.com',
             'password' => Hash::make('Lima2020.'),
-            'type_user_id' => 3
+            'type_user_id' => 1
         ]);
 
         Customer::create([
