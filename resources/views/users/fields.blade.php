de3574edf29a4a6fb622b2ae79e6be1a745a5dea
Efren Carrillo <efren.sistemas@gmail.com>
2020-08-01 02:28:33 -0500

    modulos usuarios

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -3,11 +3,16 @@
 
 <!-- User Name Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('user_name', 'Nombre Usuario:') !!}
-    {!! Form::text('user_name', null, ['class' => 'form-control','maxlength' => 150]) !!}
+    {!! Form::label('name', 'Nombre Usuario:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 150]) !!} 
+    
 </div>
-
-<!-- User Id Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('name', 'Nombre Usuario:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 150]) !!} 
+    
+</div>
+<!--  Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('user_id', 'Usuario Padre:') !!}
     {!! Form::select('user_id', $userItems, null, ['class' => 'form-control']) !!}
